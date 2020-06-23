00c00df543e760eddfbf7e5d646b9985987a169e
tomgs <tincopper@foxmail.com>
2020-06-24 00:23:20 +0800

    add scheduler module

---
+++ hodor-scheduler/hodor-scheduler-api/src/main/java/org/dromara/hodor/scheduler/api/HodorScheduler.java
@@ -0,0 +1,12 @@
+package org.dromara.hodor.scheduler.api;
+
+import org.dromara.hodor.common.extension.SPI;
+
+/**
+ * @author tomgs
+ * @version 2020/6/24 1.0
+ */
+@SPI("scheduler")
+public interface HodorScheduler {
+
+}
---
+++ hodor-scheduler/hodor-scheduler-quartz/src/main/java/org/dromara/hodor/scheduler/quartz/QuartzScheduler.java
@@ -0,0 +1,15 @@
+package org.dromara.hodor.scheduler.quartz;
+
+import org.dromara.hodor.common.extension.Join;
+import org.dromara.hodor.scheduler.api.HodorScheduler;
+
+/**
+ *  implements scheduler by quartz
+ *
+ * @author tomgs
+ * @version 2020/6/24 1.0 
+ */
+@Join
+public class QuartzScheduler implements HodorScheduler {
+    
+}
