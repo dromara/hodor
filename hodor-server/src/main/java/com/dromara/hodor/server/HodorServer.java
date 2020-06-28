fcdcac41444a4c66255ac44e222277071acfe264
zy_tang <zy_tang@kingdee.com>
2020-06-28 17:26:14 +0800

    config hodor server

--- hodor-server/src/main/java/com/dromara/hodor/server/HodorServer.java
+++ hodor-server/src/main/java/com/dromara/hodor/server/HodorServer.java
@@ -1,14 +1,25 @@
 package com.dromara.hodor.server;
 
+import javax.annotation.PostConstruct;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  *  hodor server
  *
  * @author tomgs
  * @version 2020/6/24 1.0 
  */
+@SpringBootApplication
 public class HodorServer {
 
     public static void main(String[] args) {
+        SpringApplication.run(HodorServer.class, args);
+    }
+
+    @PostConstruct
+    public void run(){
+        // start hodor server
 
     }
 
