fd39d31936c482881ba2b4abd7438e6e27ae6a95
tomgs <tincopper@foxmail.com>
2020-06-29 00:55:32 +0800

    remote useless code

--- hodor-server/src/main/java/com/dromara/hodor/server/HodorServerInit.java
+++ hodor-server/src/main/java/com/dromara/hodor/server/HodorServerInit.java
@@ -24,7 +24,7 @@ public class HodorServerInit implements ApplicationRunner {
     }
 
     @Override
-    public void run(ApplicationArguments args) throws Exception {
+    public void run(ApplicationArguments args) {
         // start hodor server
         // start remoting server
         remoteService.start();
