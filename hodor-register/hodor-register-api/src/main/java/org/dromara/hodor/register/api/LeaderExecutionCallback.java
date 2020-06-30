57a1fbc1ba41379596755390c0823c993a3ad3b2
zy_tang <zy_tang@kingdee.com>
2020-06-30 11:04:44 +0800

    add leader execution callback

---
+++ hodor-register/hodor-register-api/src/main/java/org/dromara/hodor/register/api/LeaderExecutionCallback.java
@@ -0,0 +1,8 @@
+package org.dromara.hodor.register.api;
+
+/**
+ * to be leader execution callback
+ */
+public interface LeaderExecutionCallback {
+    void execute() throws Exception;
+}
---
+++ hodor-server/src/main/java/com/dromara/hodor/server/service/LeaderService.java
@@ -0,0 +1,8 @@
+package com.dromara.hodor.server.service;
+
+/**
+ * @author tomgs
+ * @since 2020/6/30
+ */
+public class LeaderService {
+}
