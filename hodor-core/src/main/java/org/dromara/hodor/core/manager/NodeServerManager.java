0aa8abc87a1b534923d101b0ad0d166bbbdd6b5c
zy_tang <zy_tang@kingdee.com>
2020-07-28 14:31:33 +0800

    add file

---
+++ hodor-core/src/main/java/org/dromara/hodor/core/manager/NodeServerManager.java
@@ -0,0 +1,37 @@
+package org.dromara.hodor.core.manager;
+
+import com.google.common.collect.Sets;
+import java.util.Set;
+
+/**
+ * node server manager
+ *
+ * @author tomgs
+ * @since 2020/7/28
+ */
+public enum NodeServerManager {
+    INSTANCE;
+
+    private final Set<String> nodeIps = Sets.newConcurrentHashSet();
+
+    public static NodeServerManager getInstance() {
+        return INSTANCE;
+    }
+
+    public Set<String> getNodeServers() {
+        return nodeIps;
+    }
+
+    public void addNodeServer(String nodeIp) {
+        nodeIps.add(nodeIp);
+    }
+
+    public void removeNodeServer(String nodeIp) {
+        nodeIps.remove(nodeIp);
+    }
+
+    public void removeAllNodeServer() {
+        nodeIps.clear();
+    }
+
+}
