82bbd94b3414da4c23772ace428157fe268a2097
tomgs <136082619@qq.com>
2021-01-15 18:25:04 +0800

    add queue

---
+++ hodor-common/src/main/java/org/dromara/hodor/common/queue/HodorExecutor.java
@@ -0,0 +1,71 @@
+package org.dromara.hodor.common.queue;
+
+import org.dromara.hodor.common.concurrent.HodorQueueConsumerFactory;
+import org.dromara.hodor.common.disruptor.QueueConsumerExecutor;
+import org.dromara.hodor.common.disruptor.QueueConsumerFactory;
+import org.dromara.hodor.common.disruptor.QueueProviderManager;
+
+/**
+ * hodor executor
+ *
+ * @author tomgs
+ * @since 2021/1/7
+ */
+public class HodorExecutor<T> {
+    /**
+     * 分区大小.
+     */
+    private volatile Integer ringBufferSize;
+
+    /**
+     * 线程的大小.
+     */
+    private volatile Integer threadSize;
+
+    /**
+     * 消费端的数量.
+     */
+    private volatile Integer consumerSize;
+
+    /**
+     * 队列管理器
+     */
+    private QueueProviderManager<T> manager;
+
+    public HodorExecutor(int threadSize) {
+        this(new HodorQueueConsumerFactory<>("hodor_thread"), threadSize);
+    }
+
+    public HodorExecutor(QueueConsumerFactory<T> factory, int threadSize) {
+        this(factory, threadSize, threadSize);
+    }
+
+    public HodorExecutor(QueueConsumerFactory<T> factory, int threadSize, int consumerSize) {
+        this(factory, threadSize, consumerSize, 8 * 1024 * 1024);
+    }
+
+    public HodorExecutor(QueueConsumerFactory<T> factory, int threadSize, int consumerSize, int ringBufferSize) {
+        this.threadSize = threadSize;
+        this.consumerSize = consumerSize;
+        this.ringBufferSize = ringBufferSize;
+        this.manager = new QueueProviderManager<>(factory, threadSize, consumerSize, ringBufferSize);
+        this.manager.start();
+    }
+
+    public void execute(final QueueConsumerExecutor<T> consumerExecutor) {
+        this.manager.getProvider().onData(e -> e.setData(consumerExecutor.getData()));
+    }
+
+    public Integer getRingBufferSize() {
+        return ringBufferSize;
+    }
+
+    public Integer getThreadSize() {
+        return threadSize;
+    }
+
+    public Integer getConsumerSize() {
+        return consumerSize;
+    }
+
+}
---
+++ hodor-common/src/main/java/org/dromara/hodor/common/queue/HodorQueueConsumerFactory.java
@@ -0,0 +1,37 @@
+package org.dromara.hodor.common.queue;
+
+import org.dromara.hodor.common.disruptor.QueueConsumerExecutor;
+import org.dromara.hodor.common.disruptor.QueueConsumerFactory;
+
+/**
+ * HodorJobConsumerFactory
+ *
+ * @author tomgs
+ * @since 2020/9/22
+ */
+public class HodorQueueConsumerFactory<T> implements QueueConsumerFactory<T> {
+
+    private final String fixName;
+
+    private final QueueConsumerExecutor<T> executor;
+
+    public HodorQueueConsumerFactory(final String fixName) {
+        this(fixName, null);
+    }
+
+    public HodorQueueConsumerFactory(final String fixName, final QueueConsumerExecutor<T> executor) {
+        this.fixName = fixName;
+        this.executor = executor;
+    }
+
+    @Override
+    public QueueConsumerExecutor<T> create() {
+        return this.executor;
+    }
+
+    @Override
+    public String fixName() {
+        return fixName;
+    }
+
+}
