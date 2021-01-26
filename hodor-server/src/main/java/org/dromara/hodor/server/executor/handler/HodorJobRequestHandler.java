719ccd8d31913b1d28e51920f7dba2b8a79d6354
tomgs <136082619@qq.com>
2021-01-26 17:34:26 +0800

    update code format

--- hodor-server/src/main/java/org/dromara/hodor/server/executor/handler/HodorJobRequestHandler.java
+++ hodor-server/src/main/java/org/dromara/hodor/server/executor/handler/HodorJobRequestHandler.java
@@ -24,53 +24,53 @@ import org.dromara.hodor.server.service.RemotingClientService;
 @Slf4j
 public class HodorJobRequestHandler extends HodorRunnable {
 
-  private final RemotingClientService clientService;
+    private final RemotingClientService clientService;
 
-  private final RegisterService registerService;
+    private final RegisterService registerService;
 
-  private final HodorJobExecutionContext context;
+    private final HodorJobExecutionContext context;
 
-  public HodorJobRequestHandler(final HodorJobExecutionContext context) {
-    ServiceProvider serviceProvider = ServiceProvider.getInstance();
-    this.clientService = serviceProvider.getBean(RemotingClientService.class);
-    this.registerService = serviceProvider.getBean(RegisterService.class);
-    this.context = context;
-  }
-
-  @Override
-  public void execute() {
-    log.info("hodor job request handler, info {}.", context);
-    RemotingRequest<RequestBody> request = getRequestBody(context);
-    List<Host> hosts = registerService.getAvailableHosts(context);
-    for (int i = hosts.size() - 1; i >= 0; i--) {
-      try {
-        clientService.sendRequest(hosts.get(i), request);
-        break;
-      } catch (Exception e) {
-        log.error(e.getMessage(), e);
-      }
+    public HodorJobRequestHandler(final HodorJobExecutionContext context) {
+        ServiceProvider serviceProvider = ServiceProvider.getInstance();
+        this.clientService = serviceProvider.getBean(RemotingClientService.class);
+        this.registerService = serviceProvider.getBean(RegisterService.class);
+        this.context = context;
     }
-  }
 
-  @Override
-  public void exceptionCaught(Exception e) {
-    log.error(e.getMessage(), e);
-    //TODO: exception handler
-  }
+    @Override
+    public void execute() {
+        log.info("hodor job request handler, info {}.", context);
+        RemotingRequest<RequestBody> request = getRequestBody(context);
+        List<Host> hosts = registerService.getAvailableHosts(context);
+        for (int i = hosts.size() - 1; i >= 0; i--) {
+            try {
+                clientService.sendRequest(hosts.get(i), request);
+                break;
+            } catch (Exception e) {
+                log.error(e.getMessage(), e);
+            }
+        }
+    }
 
-  private RemotingRequest<RequestBody> getRequestBody(final HodorJobExecutionContext context) {
-    return RemotingRequest.builder()
-        .header(getHeader())
-        .body(SchedulerRequestBody.fromContext(context))
-        .build();
-  }
+    @Override
+    public void exceptionCaught(Exception e) {
+        log.error(e.getMessage(), e);
+        //TODO: exception handler
+    }
 
-  private Header getHeader() {
-    return Header.builder()
-        .crcCode(RemotingConst.RPC_CRC_CODE)
-        .version(RemotingConst.RPC_VERSION)
-        .type(RequestType.JOB_EXEC_REQUEST.getCode())
-        .build();
-  }
+    private RemotingRequest<RequestBody> getRequestBody(final HodorJobExecutionContext context) {
+        return RemotingRequest.builder()
+            .header(getHeader())
+            .body(SchedulerRequestBody.fromContext(context))
+            .build();
+    }
+
+    private Header getHeader() {
+        return Header.builder()
+            .crcCode(RemotingConst.RPC_CRC_CODE)
+            .version(RemotingConst.RPC_VERSION)
+            .type(RequestType.JOB_EXEC_REQUEST.getCode())
+            .build();
+    }
 
 }
