5f50f5192f121699fdcb6b95da5d6dddf964c5a9
tomgs <136082619@qq.com>
2021-04-02 18:34:13 +0800

    add flow job step

--- hodor-server/src/main/java/org/dromara/hodor/server/executor/FlowJobExecutor.java
+++ hodor-server/src/main/java/org/dromara/hodor/server/executor/FlowJobExecutor.java
@@ -8,11 +8,16 @@ import org.dromara.hodor.scheduler.api.HodorJobExecutionContext;
  * @author tomgs
  * @version 2020/6/25 1.0 
  */
-public class WorkFlowJobExecutor extends CommonJobExecutor {
+public class FlowJobExecutor extends CommonJobExecutor {
 
     @Override
     public void process(HodorJobExecutionContext context) {
         //TODO: 校验一些与工作流相关的事项
+        // 1、判断是否有正在运行的flow
+        // 2、没有则获取flow json信息
+        // 3、构建dag对象
+        // 4、存储dag对象详情
+        // 5、提交运行dag
         super.process(context);
     }
 
