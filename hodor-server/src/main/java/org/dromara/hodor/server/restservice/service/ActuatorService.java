afd9f8492bc602fa9e18a9b060fb57ad263b064e
tomgs <136082619@qq.com>
2021-08-03 15:53:56 +0800

    add actuator offline

--- hodor-server/src/main/java/org/dromara/hodor/server/restservice/service/ActuatorService.java
+++ hodor-server/src/main/java/org/dromara/hodor/server/restservice/service/ActuatorService.java
@@ -1,5 +1,6 @@
 package org.dromara.hodor.server.restservice.service;
 
+import com.google.common.base.Preconditions;
 import org.dromara.hodor.model.actuator.ActuatorInfo;
 import org.dromara.hodor.model.common.HodorResult;
 import org.dromara.hodor.server.restservice.HodorRestService;
@@ -24,16 +25,22 @@ public class ActuatorService {
 
     @RestMethod("heartbeat")
     public HodorResult<String> heartbeat(ActuatorInfo actuatorInfo) {
+        checkActuatorInfo(actuatorInfo);
         actuatorInfo.setLastHeartbeat(System.currentTimeMillis());
-        //actuatorManager.addActuator(actuatorInfo);
         registerService.createActuator(actuatorInfo);
         return HodorResult.success("success");
     }
 
     @RestMethod("offline")
     public HodorResult<String> offline(ActuatorInfo actuatorInfo) {
-        //actuatorManager.removeActuator(actuatorInfo);
+        checkActuatorInfo(actuatorInfo);
+        registerService.removeActuator(actuatorInfo);
         return HodorResult.success("success");
+    }
+
+    private void checkActuatorInfo(ActuatorInfo actuatorInfo) {
+        Preconditions.checkNotNull(actuatorInfo.getNodeInfo(), "actuator node info must be not null.");
+        Preconditions.checkNotNull(actuatorInfo.getGroupNames(), "actuator group names must be not null.");
     }
 
 }
