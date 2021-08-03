c210adfc642abad8171761198c83e39d5bdd1c5d
tomgs <136082619@qq.com>
2021-08-03 19:15:29 +0800

    add cron utils

---
+++ hodor-common/src/main/java/org/dromara/hodor/common/cron/CronUtils.java
@@ -0,0 +1,16 @@
+package org.dromara.hodor.common.cron;
+
+/**
+ * cron utils
+ *
+ * @author tomgs
+ * @since 2021/8/3
+ */
+public class CronUtils {
+
+    /**
+     * cron disabled这个用于不需要调度，但是需要注册的任务，可能用于任务的关联任务
+     */
+    public static final String CRON_DISABLED = "-";
+
+}
