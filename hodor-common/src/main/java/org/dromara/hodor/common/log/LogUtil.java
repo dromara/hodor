37ec2e3c427588bda48e7d6fddcbce2828919601
tomgs <136082619@qq.com>
2021-08-10 20:58:21 +0800

    update LogUtil

--- hodor-common/src/main/java/org/dromara/hodor/common/log/LogUtil.java
+++ hodor-common/src/main/java/org/dromara/hodor/common/log/LogUtil.java
@@ -1,12 +1,17 @@
 package org.dromara.hodor.common.log;
 
 import java.io.File;
+import java.util.function.Supplier;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.Appender;
 import org.apache.logging.log4j.core.Layout;
 import org.apache.logging.log4j.core.LoggerContext;
 import org.apache.logging.log4j.core.appender.FileAppender;
+import org.apache.logging.log4j.core.appender.RollingFileAppender;
+import org.apache.logging.log4j.core.appender.rolling.DefaultRolloverStrategy;
+import org.apache.logging.log4j.core.appender.rolling.TimeBasedTriggeringPolicy;
 import org.apache.logging.log4j.core.config.AppenderRef;
 import org.apache.logging.log4j.core.config.Configuration;
 import org.apache.logging.log4j.core.config.LoggerConfig;
@@ -22,7 +27,7 @@ public class LogUtil {
 
     private static final LogUtil INSTANCE = new LogUtil();
 
-    private static final String DEFAULT_LAYOUT = "[%d{yyyy-MM-dd HH:mm:ss}] [%thread] %-5level %class{40}:%L %msg%n";
+    public static final String DEFAULT_LAYOUT = "[%d{yyyy-MM-dd HH:mm:ss}] [%thread] %-5level %class{40}:%L %msg%n";
 
     private final LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
     private final Configuration config = ctx.getConfiguration();
@@ -40,33 +45,64 @@ public class LogUtil {
     }
 
     public Logger createLogger(String loggerName, File logFile, String layoutPattern) {
+        return createLogger(loggerName, () -> buildFileAppender(loggerName, logFile, layoutPattern));
+    }
+
+    public Logger createRollingLogger(String loggerName, File logFile, String layoutPattern, int interval) {
+        return createLogger(loggerName, () -> buildRollingFileAppender(loggerName, logFile, layoutPattern, interval));
+    }
+
+    public Logger createLogger(String loggerName, Supplier<Appender> appenderSupplier) {
         Logger logger = LogManager.getLogger(loggerName);
-        attachFileAppender(logger, logFile, layoutPattern);
+        Appender appender = appenderSupplier.get();
+        appender.start();
+        attachFileAppend(logger, appender);
         return logger;
     }
 
-    public void attachFileAppender(Logger logger, File logFile, String layoutPattern) {
+    public void attachFileAppend(Logger logger, Appender appender) {
         String loggerName = logger.getName();
+        AppenderRef ref = AppenderRef.createAppenderRef(loggerName, null, null);
+        AppenderRef[] refs = new AppenderRef[] { ref };
+
+        LoggerConfig loggerConfig = LoggerConfig.createLogger(false, Level.ALL, loggerName, "true", refs, null, config, null);
+        loggerConfig.addAppender(appender, null, null);
+
+        config.addAppender(appender);
+        config.addLogger(loggerName, loggerConfig);
+
+        ctx.updateLoggers();
+    }
+
+    public Appender buildFileAppender(String loggerName, File logFile, String layoutPattern) {
         Layout<String> layout = PatternLayout.newBuilder().withPattern(layoutPattern).build();
-        FileAppender fileAppender = FileAppender.newBuilder()
+        return FileAppender.newBuilder()
             .setConfiguration(config)
             .withAppend(true)
             .withName(loggerName)
             .withFileName(logFile.getAbsolutePath())
             .withLayout(layout)
             .build();
-        fileAppender.start();
+    }
 
-        AppenderRef ref = AppenderRef.createAppenderRef(loggerName, null, null);
-        AppenderRef[] refs = new AppenderRef[] { ref };
-
-        LoggerConfig loggerConfig = LoggerConfig.createLogger(false, Level.ALL, loggerName, "true", refs, null, config, null);
-        loggerConfig.addAppender(fileAppender, null, null);
-
-        config.addAppender(fileAppender);
-        config.addLogger(loggerName, loggerConfig);
-
-        ctx.updateLoggers();
+    public Appender buildRollingFileAppender(String loggerName, File logFile, String layoutPattern, int interval) {
+        Layout<String> layout = PatternLayout.newBuilder().withPattern(layoutPattern).build();
+        String fileName = logFile.getAbsolutePath();
+        return RollingFileAppender.newBuilder()
+            .setConfiguration(config)
+            .withAppend(true)
+            .withName(loggerName)
+            .withFileName(fileName)
+            .withFilePattern(fileName + "_%d{yyyy-MM-dd-HH-mm-ss}_%i")
+            .withLayout(layout)
+            .withPolicy(TimeBasedTriggeringPolicy.newBuilder()
+                .withInterval(interval) // unit seconds
+                .withModulate(true)
+                .build())
+            .withStrategy(DefaultRolloverStrategy.newBuilder()
+                .withMax("10")
+                .build())
+            .build();
     }
 
     public void stopLogger(String loggerName) {
---
+++ hodor-common/src/test/java/com/dromara/hodor/common/LogUtilTest.java
@@ -0,0 +1,37 @@
+package com.dromara.hodor.common;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+import org.apache.logging.log4j.Logger;
+import org.dromara.hodor.common.log.LogUtil;
+import org.dromara.hodor.common.utils.ThreadUtils;
+import org.junit.Test;
+
+/**
+ * log util test
+ *
+ * @author tomgs
+ * @since 2021/8/10
+ */
+public class LogUtilTest {
+
+    @Test
+    public void testCreateLogFile() {
+        File logFile = new File(System.getProperty("user.dir") + "/log/test.log");
+        Logger logger = LogUtil.getInstance().createLogger("log-test", logFile);
+        logger.info("12312312");
+        logger.info("12312312");
+    }
+
+    @Test
+    public void testCreateRollingLogFile() {
+        File logFile = new File(System.getProperty("user.dir") + "/log/test-rolling.log");
+        Logger logger = LogUtil.getInstance().createRollingLogger("log-test", logFile, "%msg%n", 1);
+        for (int i = 0; i < 3; i++) {
+            logger.info("12312312");
+            logger.info("12312312");
+            ThreadUtils.sleep(TimeUnit.SECONDS, 1);
+        }
+    }
+
+}
