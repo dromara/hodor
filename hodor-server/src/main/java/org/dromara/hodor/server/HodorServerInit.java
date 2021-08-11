c2e97b0091ca057750d335f6cb7c0e4369747d3b
tomgs <136082619@qq.com>
2021-08-11 14:10:36 +0800

    add NoNull annotation

--- hodor-server/src/main/java/org/dromara/hodor/server/HodorServerInit.java
+++ hodor-server/src/main/java/org/dromara/hodor/server/HodorServerInit.java
@@ -9,6 +9,7 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 
 /**
@@ -59,7 +60,7 @@ public class HodorServerInit implements ApplicationRunner, ApplicationContextAwa
     }
 
     @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+    public void setApplicationContext(@NonNull ApplicationContext applicationContext) throws BeansException {
         this.applicationContext = applicationContext;
     }
 
