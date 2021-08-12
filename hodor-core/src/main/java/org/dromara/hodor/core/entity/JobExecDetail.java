9e629d9b21388bc97fff99703e4d1df2626b4321
tomgs <136082619@qq.com>
2021-08-12 20:57:37 +0800

    update job detail

--- hodor-core/src/main/java/org/dromara/hodor/core/entity/JobExecDetail.java
+++ hodor-core/src/main/java/org/dromara/hodor/core/entity/JobExecDetail.java
@@ -1,10 +1,13 @@
 package org.dromara.hodor.core.entity;
 
 import com.baomidou.mybatisplus.annotation.TableName;
+import java.util.Date;
 import lombok.Data;
+import org.dromara.hodor.model.enums.JobExecuteStatus;
 
 /**
  * job executor detail info
+ *
  * @author tomgs
  * @since 2020/8/27
  */
@@ -13,5 +16,37 @@ import lombok.Data;
 public class JobExecDetail {
 
     private long id;
+
+    private String groupName;
+
+    private String jobName;
+
+    private String schedulerEndpoint;
+
+    private String actuatorEndpoint;
+
+    private Date scheduleStart;
+
+    private Date scheduleEnd;
+
+    private Date executeStart;
+
+    private Date executeEnd;
+
+    private Integer elapsedTime;
+
+    private JobExecuteStatus executeStatus;
+
+    private String comments;
+
+    private Boolean isTimeout;
+
+    private Integer encType;
+
+    private byte[] detailedLog;
+
+    private byte[] jobExeData;
+
+    private String parentRequestId;
 
 }
