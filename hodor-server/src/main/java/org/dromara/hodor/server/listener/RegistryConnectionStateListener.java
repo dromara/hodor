f3cff7dc503d11c944e22735567d729eab2cdd10
tomgs <136082619@qq.com>
2021-08-18 20:37:26 +0800

    add ConnectionState handler

---
+++ hodor-server/src/main/java/org/dromara/hodor/server/listener/RegistryConnectionStateListener.java
@@ -0,0 +1,37 @@
+package org.dromara.hodor.server.listener;
+
+import lombok.extern.slf4j.Slf4j;
+import org.dromara.hodor.register.api.ConnectionState;
+import org.dromara.hodor.register.api.ConnectionStateChangeListener;
+import org.dromara.hodor.server.service.RegistryService;
+
+/**
+ * RegisterConnectionStateListener
+ * @author tomgs
+ * @since 2021/8/18
+ */
+@Slf4j
+public class RegistryConnectionStateListener implements ConnectionStateChangeListener {
+
+    private final RegistryService registryService;
+
+    public RegistryConnectionStateListener(final RegistryService registryService) {
+        this.registryService = registryService;
+    }
+
+    @Override
+    public void stateChanged(ConnectionState newState) {
+        if (ConnectionState.SUSPENDED == newState || ConnectionState.LOST == newState) {
+            log.error("scheduler {} connection {} from registry.", registryService.getServerEndpoint(), newState);
+            // reconnect
+            // registryService.start();
+            // pause scheduler
+            // TODO: pause scheduler
+        } else if (ConnectionState.RECONNECTED == newState) {
+            log.info("scheduler {} reconnected registry.", registryService.getServerEndpoint());
+            registryService.initNode();
+            // TODO: resume scheduler
+        }
+    }
+
+}
