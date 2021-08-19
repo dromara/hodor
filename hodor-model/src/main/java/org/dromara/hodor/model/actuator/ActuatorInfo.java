49b618bff4e3f37e35f3868264cfb7d3a4e2eda8
tomgs <136082619@qq.com>
2021-08-19 16:25:12 +0800

    class ActuatorInfo add nodeEndpoint field

--- hodor-model/src/main/java/org/dromara/hodor/model/actuator/ActuatorInfo.java
+++ hodor-model/src/main/java/org/dromara/hodor/model/actuator/ActuatorInfo.java
@@ -1,6 +1,9 @@
 package org.dromara.hodor.model.actuator;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.dromara.hodor.model.node.NodeInfo;
 
 import java.util.Set;
@@ -12,9 +15,14 @@ import java.util.Set;
  * @version 2021/8/1 1.0 
  */
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ActuatorInfo {
 
     private NodeInfo nodeInfo;
+
+    private String nodeEndpoint;
 
     private Set<String> groupNames;
 
