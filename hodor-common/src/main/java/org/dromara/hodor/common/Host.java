a36da644b5d10374676ebff17bfdb35363f96ef2
tomgs <136082619@qq.com>
2021-08-24 20:19:52 +0800

    update host builder method

--- hodor-common/src/main/java/org/dromara/hodor/common/Host.java
+++ hodor-common/src/main/java/org/dromara/hodor/common/Host.java
@@ -1,9 +1,10 @@
 package org.dromara.hodor.common;
 
 import java.util.List;
-import lombok.Builder;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import org.dromara.hodor.common.utils.StringUtils;
 
 /**
@@ -13,7 +14,8 @@ import org.dromara.hodor.common.utils.StringUtils;
  * @since 2020/9/24
  */
 @Data
-@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @EqualsAndHashCode
 public class Host {
 
@@ -37,6 +39,47 @@ public class Host {
 
         }
         return Host.builder().endpoint(endpoint).ip(hostSplit.get(0)).port(Integer.parseInt(hostSplit.get(1))).build();
+    }
+
+    public static Host.HostBuilder builder() {
+        return new Host.HostBuilder();
+    }
+
+    public static class HostBuilder {
+        private String endpoint;
+
+        private String ip;
+
+        private int port;
+
+        HostBuilder() {
+        }
+
+        public Host.HostBuilder endpoint(final String endpoint) {
+            this.endpoint = endpoint;
+            return this;
+        }
+
+        public Host.HostBuilder ip(final String ip) {
+            this.ip = ip;
+            return this;
+        }
+
+        public Host.HostBuilder port(final int port) {
+            this.port = port;
+            return this;
+        }
+
+        public Host build() {
+            if (this.endpoint == null) {
+                this.endpoint = this.ip + ":" + this.port;
+            }
+            return new Host(this.endpoint, this.ip, this.port);
+        }
+
+        public String toString() {
+            return "Host.HostBuilder(endpoint=" + this.endpoint + ", ip=" + this.ip + ", port=" + this.port + ")";
+        }
     }
 
 }
