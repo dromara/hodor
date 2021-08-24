a7bad7c15c8cd4f009570e9efe32e543ece11134
tomgs <136082619@qq.com>
2021-08-24 20:47:09 +0800

    update DataInterval

--- hodor-model/src/main/java/org/dromara/hodor/model/scheduler/DataInterval.java
+++ hodor-model/src/main/java/org/dromara/hodor/model/scheduler/DataInterval.java
@@ -3,6 +3,7 @@ package org.dromara.hodor.model.scheduler;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 /**
