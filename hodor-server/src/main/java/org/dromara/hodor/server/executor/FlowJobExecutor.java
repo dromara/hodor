c3bb86f02776a63ee5d629de4e1ca32101183a9c
tomgs <136082619@qq.com>
2021-08-31 20:32:55 +0800

    update flow job executor

--- hodor-server/src/main/java/org/dromara/hodor/server/executor/FlowJobExecutor.java
+++ hodor-server/src/main/java/org/dromara/hodor/server/executor/FlowJobExecutor.java
@@ -1,5 +1,6 @@
 package org.dromara.hodor.server.executor;
 
+import org.dromara.hodor.model.job.JobKey;
 import org.dromara.hodor.scheduler.api.HodorJobExecutionContext;
 
 /**
@@ -18,7 +19,26 @@ public class FlowJobExecutor extends CommonJobExecutor {
         // 3、构建dag对象
         // 4、存储dag对象详情
         // 5、提交运行dag
+        if (isAlreadyRunningJob(context.getJobKey())) {
+            return;
+        }
+
+        /*if (isAlreadyRunning(execId)) {
+            return;
+        }
+        final long tsBeforeFlowRunnerCreation = System.currentTimeMillis();
+        final FlowRunner runner = createFlowRunner(execId);
+        runner.setFlowCreateTime(System.currentTimeMillis() - tsBeforeFlowRunnerCreation);
+        // Check again.
+        if (isAlreadyRunning(execId)) {
+            return;
+        }
+        submitFlowRunner(runner);*/
         super.process(context);
+    }
+
+    private boolean isAlreadyRunningJob(JobKey jobKey) {
+        return false;
     }
 
 }
