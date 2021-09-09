c8b9885e6ed68b3a959259080a954e4b3163aaba
tomgs <136082619@qq.com>
2021-09-09 19:58:18 +0800

    add rootJobKey field

--- hodor-scheduler/hodor-scheduler-api/src/main/java/org/dromara/hodor/scheduler/api/HodorJobExecutionContext.java
+++ hodor-scheduler/hodor-scheduler-api/src/main/java/org/dromara/hodor/scheduler/api/HodorJobExecutionContext.java
@@ -6,22 +6,28 @@ import org.dromara.hodor.model.job.JobDesc;
 import org.dromara.hodor.model.job.JobKey;
 
 /**
- *  hodor scheduler context
+ * hodor scheduler context
  *
  * @author tomgs
- * @version 2020/6/25 1.0 
+ * @version 2020/6/25 1.0
  */
 public class HodorJobExecutionContext {
 
     private final long requestId;
+
     private final String schedulerName;
+
+    private final JobKey rootJobKey;
+
     private final JobKey jobKey;
+
     private final JobDesc jobDesc;
+
     private final Date fireTime;
 
-    public HodorJobExecutionContext(final JobDesc jobDesc,
-                                    final String schedulerName,
-                                    final Date fireTime) {
+    public HodorJobExecutionContext(final JobKey rootJobKey, final JobDesc jobDesc,
+                                    final String schedulerName, final Date fireTime) {
+        this.rootJobKey = rootJobKey;
         this.requestId = IdGenerator.defaultGenerator().nextId();
         this.schedulerName = schedulerName;
         this.jobDesc = jobDesc;
@@ -30,18 +36,22 @@ public class HodorJobExecutionContext {
     }
 
     public HodorJobExecutionContext(final long requestId,
-                                    final JobDesc jobDesc,
-                                    final String schedulerName,
-                                    final Date fireTime) {
+                                    final JobKey rootJobKey, final JobDesc jobDesc,
+                                    final String schedulerName, final Date fireTime) {
         this.requestId = requestId;
         this.schedulerName = schedulerName;
         this.jobDesc = jobDesc;
         this.jobKey = JobKey.of(jobDesc.getGroupName(), jobDesc.getJobName());
+        this.rootJobKey = rootJobKey;
         this.fireTime = fireTime;
     }
 
     public long getRequestId() {
         return requestId;
+    }
+
+    public JobKey getRootJobKey() {
+        return rootJobKey;
     }
 
     public JobKey getJobKey() {
