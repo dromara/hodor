ad20f1b227a07d8e72d914bc48b88f4b39234813
tomgs <136082619@qq.com>
2021-09-18 16:18:56 +0800

    add valueOf method

--- hodor-model/src/main/java/org/dromara/hodor/model/enums/Priority.java
+++ hodor-model/src/main/java/org/dromara/hodor/model/enums/Priority.java
@@ -8,20 +8,29 @@ package org.dromara.hodor.model.enums;
  */
 public enum Priority {
 
-  DEFAULT(0),
+    DEFAULT(0),
 
-  MEDIUM(1),
+    MEDIUM(1),
 
-  HIGHER(2);
+    HIGHER(2);
 
-  private final int priority;
+    private final int priority;
 
-  Priority(int priority) {
-    this.priority = priority;
-  }
+    Priority(int priority) {
+        this.priority = priority;
+    }
 
-  public int getValue() {
-    return priority;
-  }
+    public int getValue() {
+        return priority;
+    }
+
+    public static Priority valueOf(int priorityValue) {
+        for (Priority priority : Priority.values()) {
+            if (priority.priority == priorityValue) {
+                return priority;
+            }
+        }
+        throw new IllegalArgumentException("not found priority value " + priorityValue);
+    }
 
 }
