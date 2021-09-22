9b8eb257d4f01a670722fee390333c93e6e936f9
tomgs <136082619@qq.com>
2021-09-22 15:52:48 +0800

    add flow job test case

---
+++ hodor-core/src/test/java/org/dromara/hodor/core/service/FlowJobInfoServiceTest.java
@@ -0,0 +1,56 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.dromara.hodor.core.service;
+
+import java.io.File;
+import java.net.URL;
+import javax.annotation.Resource;
+import org.dromara.hodor.core.BaseTest;
+import org.dromara.hodor.core.dag.FlowData;
+import org.dromara.hodor.core.dag.FlowDataLoader;
+import org.junit.Test;
+
+/**
+ * FlowJobInfoServiceTest
+ *
+ * @author tomgs
+ * @since 2021/9/22
+ */
+public class FlowJobInfoServiceTest extends BaseTest {
+
+    @Resource
+    private FlowJobInfoService flowJobInfoService;
+
+    @Test
+    public void testAddFlowJob() throws Exception {
+        File file = loadFlowFileFromResource();
+        FlowDataLoader loader = new FlowDataLoader();
+        FlowData flowData = loader.load(file);
+
+        flowJobInfoService.addFlowJob(flowData);
+    }
+
+    private File loadFlowFileFromResource() {
+        final ClassLoader loader = getClass().getClassLoader();
+        //ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        URL resource = loader.getResource("hello_world_flow.yaml");
+        assert resource != null;
+        return new File(resource.getFile());
+    }
+
+}
