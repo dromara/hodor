8a20e46a0f51df3e53a7a3af3b810ae0cf9a94aa
tomgs <136082619@qq.com>
2021-09-24 16:31:53 +0800

    update flow job response handler

--- hodor-server/src/main/java/org/dromara/hodor/server/executor/handler/HodorFlowJobResponseHandler.java
+++ hodor-server/src/main/java/org/dromara/hodor/server/executor/handler/HodorFlowJobResponseHandler.java
@@ -7,6 +7,7 @@ import org.dromara.hodor.common.dag.Node;
 import org.dromara.hodor.common.dag.Status;
 import org.dromara.hodor.common.event.AbstractEventPublisher;
 import org.dromara.hodor.common.event.Event;
+import org.dromara.hodor.model.enums.JobExecuteStatus;
 import org.dromara.hodor.model.job.JobKey;
 import org.dromara.hodor.remoting.api.message.RemotingResponse;
 import org.dromara.hodor.remoting.api.message.RemotingStatus;
@@ -49,6 +50,9 @@ public class HodorFlowJobResponseHandler extends AbstractEventPublisher<Tuple2<J
         Tuple2<JobKey, JobExecuteResponse> tuple = event.getValue();
         JobKey rootJobKey = tuple.getFirst();
         JobExecuteResponse jobExecuteResponse = tuple.getSecond();
+        if (!JobExecuteStatus.isFinished(jobExecuteResponse.getStatus())) {
+            return;
+        }
         try {
             // notify execute next
             Dag dagInstance = flowJobExecutorManager.getDagInstance(rootJobKey);
