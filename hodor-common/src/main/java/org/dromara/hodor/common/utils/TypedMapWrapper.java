7a0502ee6befbd6bffac6e10a6f2da18245499f1
tomgs <136082619@qq.com>
2021-09-24 16:08:33 +0800

    TypedMapWrapper add convertInstance method

--- hodor-common/src/main/java/org/dromara/hodor/common/utils/TypedMapWrapper.java
+++ hodor-common/src/main/java/org/dromara/hodor/common/utils/TypedMapWrapper.java
@@ -16,157 +16,178 @@
 
 package org.dromara.hodor.common.utils;
 
+import cn.hutool.core.util.ReflectUtil;
+import java.lang.reflect.Field;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import org.dromara.hodor.common.exception.ConvertException;
 
 public class TypedMapWrapper<K, V> {
 
-  private final Map<K, V> map;
+    private final Map<K, V> map;
 
-  public TypedMapWrapper(final Map<K, V> map) {
-    this.map = map;
-  }
-
-  public String getString(final K key) {
-    return getString(key, null);
-  }
-
-  public String getString(final K key, final String defaultVal) {
-    final Object obj = this.map.get(key);
-    if (obj == null) {
-      return defaultVal;
-    }
-    if (obj instanceof String) {
-      return (String) obj;
+    public TypedMapWrapper(final Map<K, V> map) {
+        this.map = map;
     }
 
-    return obj.toString();
-  }
-
-  public Boolean getBool(final K key, final Boolean defaultVal) {
-    final Object obj = this.map.get(key);
-    if (obj == null) {
-      return defaultVal;
+    public String getString(final K key) {
+        return getString(key, null);
     }
 
-    return (Boolean) obj;
-  }
+    public String getString(final K key, final String defaultVal) {
+        final Object obj = this.map.get(key);
+        if (obj == null) {
+            return defaultVal;
+        }
+        if (obj instanceof String) {
+            return (String) obj;
+        }
 
-  public Integer getInt(final K key) {
-    return getInt(key, -1);
-  }
-
-  public Integer getInt(final K key, final Integer defaultVal) {
-    final Object obj = this.map.get(key);
-    if (obj == null) {
-      return defaultVal;
-    }
-    if (obj instanceof Integer) {
-      return (Integer) obj;
-    } else if (obj instanceof String) {
-      return Integer.valueOf((String) obj);
-    } else {
-      return defaultVal;
-    }
-  }
-
-  public Long getLong(final K key) {
-    return getLong(key, -1L);
-  }
-
-  public Long getLong(final K key, final Long defaultVal) {
-    final Object obj = this.map.get(key);
-    if (obj == null) {
-      return defaultVal;
-    }
-    if (obj instanceof Long) {
-      return (Long) obj;
-    } else if (obj instanceof Integer) {
-      return Long.valueOf((Integer) obj);
-    } else if (obj instanceof String) {
-      return Long.valueOf((String) obj);
-    } else {
-      return defaultVal;
-    }
-  }
-
-  public Double getDouble(final K key) {
-    return getDouble(key, -1.0d);
-  }
-
-  public Double getDouble(final K key, final Double defaultVal) {
-    final Object obj = this.map.get(key);
-    if (obj == null) {
-      return defaultVal;
-    }
-    if (obj instanceof Double) {
-      return (Double) obj;
-    } else if (obj instanceof String) {
-      return Double.valueOf((String) obj);
-    } else {
-      return defaultVal;
-    }
-  }
-
-  @SuppressWarnings("unchecked")
-  public Collection<String> getStringCollection(final K key) {
-    final Object obj = this.map.get(key);
-    return (Collection<String>) obj;
-  }
-
-  @SuppressWarnings("unchecked")
-  public Collection<String> getStringCollection(final K key,
-      final Collection<String> defaultVal) {
-    final Object obj = this.map.get(key);
-    if (obj == null) {
-      return defaultVal;
+        return obj.toString();
     }
 
-    return (Collection<String>) obj;
-  }
+    public Boolean getBool(final K key, final Boolean defaultVal) {
+        final Object obj = this.map.get(key);
+        if (obj == null) {
+            return defaultVal;
+        }
 
-  @SuppressWarnings("unchecked")
-  public <C> Collection<C> getCollection(final K key) {
-    final Object obj = this.map.get(key);
-    if (obj instanceof Collection) {
-      return (Collection<C>) obj;
+        return (Boolean) obj;
     }
-    return null;
-  }
 
-  @SuppressWarnings("unchecked")
-  public <L> List<L> getList(final K key) {
-    final Object obj = this.map.get(key);
-    if (obj instanceof List) {
-      return (List<L>) obj;
+    public Integer getInt(final K key) {
+        return getInt(key, -1);
     }
-    return null;
-  }
 
-  @SuppressWarnings("unchecked")
-  public <L> List<L> getList(final K key, final List<L> defaultVal) {
-    final Object obj = this.map.get(key);
-    if (obj instanceof List) {
-      return (List<L>) obj;
+    public Integer getInt(final K key, final Integer defaultVal) {
+        final Object obj = this.map.get(key);
+        if (obj == null) {
+            return defaultVal;
+        }
+        if (obj instanceof Integer) {
+            return (Integer) obj;
+        } else if (obj instanceof String) {
+            return Integer.valueOf((String) obj);
+        } else {
+            return defaultVal;
+        }
     }
-    return defaultVal;
-  }
 
-  public Object getObject(final K key) {
-    return this.map.get(key);
-  }
+    public Long getLong(final K key) {
+        return getLong(key, -1L);
+    }
 
-  public Map<K, V> getMap() {
-    return this.map;
-  }
+    public Long getLong(final K key, final Long defaultVal) {
+        final Object obj = this.map.get(key);
+        if (obj == null) {
+            return defaultVal;
+        }
+        if (obj instanceof Long) {
+            return (Long) obj;
+        } else if (obj instanceof Integer) {
+            return Long.valueOf((Integer) obj);
+        } else if (obj instanceof String) {
+            return Long.valueOf((String) obj);
+        } else {
+            return defaultVal;
+        }
+    }
 
-  @SuppressWarnings("unchecked")
-  public <S, T> Map<S, T> getMap(final K key) {
-    return (Map<S, T>) this.map.get(key);
-  }
+    public Double getDouble(final K key) {
+        return getDouble(key, -1.0d);
+    }
 
-  public boolean containsKey(final K key) {
-    return this.map.containsKey(key);
-  }
+    public Double getDouble(final K key, final Double defaultVal) {
+        final Object obj = this.map.get(key);
+        if (obj == null) {
+            return defaultVal;
+        }
+        if (obj instanceof Double) {
+            return (Double) obj;
+        } else if (obj instanceof String) {
+            return Double.valueOf((String) obj);
+        } else {
+            return defaultVal;
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public Collection<String> getStringCollection(final K key) {
+        final Object obj = this.map.get(key);
+        return (Collection<String>) obj;
+    }
+
+    @SuppressWarnings("unchecked")
+    public Collection<String> getStringCollection(final K key,
+                                                  final Collection<String> defaultVal) {
+        final Object obj = this.map.get(key);
+        if (obj == null) {
+            return defaultVal;
+        }
+
+        return (Collection<String>) obj;
+    }
+
+    @SuppressWarnings("unchecked")
+    public <C> Collection<C> getCollection(final K key) {
+        final Object obj = this.map.get(key);
+        if (obj instanceof Collection) {
+            return (Collection<C>) obj;
+        }
+        return null;
+    }
+
+    @SuppressWarnings("unchecked")
+    public <L> List<L> getList(final K key) {
+        final Object obj = this.map.get(key);
+        if (obj instanceof List) {
+            return (List<L>) obj;
+        }
+        return null;
+    }
+
+    @SuppressWarnings("unchecked")
+    public <L> List<L> getList(final K key, final List<L> defaultVal) {
+        final Object obj = this.map.get(key);
+        if (obj instanceof List) {
+            return (List<L>) obj;
+        }
+        return defaultVal;
+    }
+
+    public Object getObject(final K key) {
+        return this.map.get(key);
+    }
+
+    public Map<K, V> getMap() {
+        return this.map;
+    }
+
+    @SuppressWarnings("unchecked")
+    public <S, T> Map<S, T> getMap(final K key) {
+        return (Map<S, T>) this.map.get(key);
+    }
+
+    public boolean containsKey(final K key) {
+        return this.map.containsKey(key);
+    }
+
+    @SuppressWarnings("all")
+    public <T> T convertInstance(Class<T> tClass) {
+        try {
+            T t = tClass.newInstance();
+            Field[] fields = ReflectUtil.getFields(tClass);
+            for (Field field : fields) {
+                Object fieldValue = this.map.get(field.getName());
+                if (fieldValue != null) {
+                    ReflectUtil.setFieldValue(t, field, fieldValue);
+                }
+            }
+            return t;
+        } catch (Exception e) {
+            throw new ConvertException(e);
+        }
+    }
+
 }
