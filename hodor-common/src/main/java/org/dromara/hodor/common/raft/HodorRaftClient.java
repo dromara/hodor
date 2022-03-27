d59c0460b2a4d7cf14304da82d8e6876f0c5391f
tincopper <136082619@qq.com>
2022-03-27 20:02:46 +0800

    add ratis lib

---
+++ hodor-common/src/main/java/org/dromara/hodor/common/raft/HodorRaftClient.java
@@ -0,0 +1,11 @@
+package org.dromara.hodor.common.raft;
+
+/**
+ * HodorRaftClient
+ *
+ * @author tomgs
+ * @version 2022/3/20 1.0
+ */
+public class HodorRaftClient {
+
+}
---
+++ hodor-common/src/main/java/org/dromara/hodor/common/raft/HodorRaftServer.java
@@ -0,0 +1,23 @@
+package org.dromara.hodor.common.raft;
+
+/**
+ * HodorRaftServer
+ *
+ * @author tomgs
+ * @version 2022/3/20 1.0
+ */
+public class HodorRaftServer {
+
+    public HodorRaftServer() {
+
+    }
+
+    public void start() {
+
+    }
+
+    public void stop() {
+
+    }
+
+}
