f12eb65bda2233500e574efb57ab35fed617a56b
tomgs <136082619@qq.com>
2022-03-30 20:11:44 +0800

    add raft lib

--- hodor-common/src/main/java/org/dromara/hodor/common/raft/HodorRaftServer.java
+++ hodor-common/src/main/java/org/dromara/hodor/common/raft/HodorRaftServer.java
@@ -1,5 +1,21 @@
 package org.dromara.hodor.common.raft;
 
+import cn.hutool.core.lang.Assert;
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import org.apache.ratis.conf.RaftProperties;
+import org.apache.ratis.grpc.GrpcConfigKeys;
+import org.apache.ratis.protocol.RaftGroupId;
+import org.apache.ratis.protocol.RaftPeer;
+import org.apache.ratis.server.RaftServer;
+import org.apache.ratis.server.RaftServerConfigKeys;
+import org.apache.ratis.server.impl.ServerImplUtils;
+import org.apache.ratis.statemachine.StateMachine;
+import org.apache.ratis.util.NetUtils;
+
 /**
  * HodorRaftServer
  *
@@ -8,16 +24,44 @@ package org.dromara.hodor.common.raft;
  */
 public class HodorRaftServer {
 
-    public HodorRaftServer() {
+    private static final Map<RaftGroupId, StateMachine> registry = new ConcurrentHashMap<>();
 
+    private final RaftOptions raftOptions;
+
+    private RaftServer server;
+
+    private final String endpoint;
+
+    private final File storageDir;
+
+    public HodorRaftServer(final RaftOptions raftOptions) throws IOException {
+        Assert.notNull(raftOptions, "raftOptions must not be null.");
+        this.raftOptions = raftOptions;
+        this.endpoint = raftOptions.getEndpoint();
+        this.storageDir = raftOptions.getStorageDir();
+        init();
     }
 
-    public void start() {
+    private void init() throws IOException {
+        //create a property object
+        RaftProperties properties = new RaftProperties();
 
+        RaftPeer currentPeer = GroupManager.getInstance().buildRaftPeer(NetUtils.createSocketAddr(endpoint), 0);
+        //set the storage directory (different for each peer) in RaftProperty object
+        RaftServerConfigKeys.setStorageDir(properties, Collections.singletonList(storageDir));
+        //set the port which server listen to in RaftProperty object
+        int port = NetUtils.createSocketAddr(currentPeer.getAddress()).getPort();
+        GrpcConfigKeys.Server.setPort(properties, port);
+        //create and start the Raft server
+        this.server = ServerImplUtils.newRaftServer(currentPeer.getId(), null, registry::get, properties, null);
     }
 
-    public void stop() {
+    public void start() throws IOException {
+        this.server.start();
+    }
 
+    public void stop() throws IOException {
+        this.server.close();
     }
 
 }
