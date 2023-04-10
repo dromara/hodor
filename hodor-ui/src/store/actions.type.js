ca5af90ae1a69f6a9506fc2306d604da575a55d5
tincopper <136082619@qq.com>
2023-04-10 15:15:41 +0800

    Migrate hodor-ui to a separate project

--- hodor-ui/src/store/actions.type.js
+++
@@ -1,7 +0,0 @@
-export const LOGIN = 'login'
-export const LOG_OUT = 'logout'
-export const CHECK_AUTH = 'checkAuth'
-export const GET_LOGIN_USER_INFO = 'getLoginUserInfo'
-export const CHANGE_PROJECT = 'changeProject'
-export const CHANGE_ENVIRONMENT = 'changeEnvironment'
-
