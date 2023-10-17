814ce7ee9090062f49289ca9eb2eae810d12eff2
tincopper <136082619@qq.com>
2023-10-18 00:01:04 +0800

    update agent start
    (cherry picked from commit 7c97e6b7f750467233e1d03580cf4aff891c536a)

--- hodor-actuator/hodor-actuator-agent/src/main/java/org/dromara/hodor/actuator/agent/HodorAgentActuatorInit.java
+++ hodor-actuator/hodor-actuator-agent/src/main/java/org/dromara/hodor/actuator/agent/HodorAgentActuatorInit.java
@@ -5,6 +5,8 @@ import org.dromara.hodor.actuator.api.HodorActuatorManager;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 
+import java.util.concurrent.CountDownLatch;
+
 /**
  * HodorBigdataActuatorInit
  *
@@ -16,19 +18,34 @@ public class HodorAgentActuatorInit implements ApplicationRunner {
 
     private final HodorActuatorManager actuatorManager;
 
+    private final CountDownLatch latch;
+
     public HodorAgentActuatorInit(final HodorActuatorManager actuatorManager) {
         this.actuatorManager = actuatorManager;
+        this.latch = new CountDownLatch(1);
     }
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
         log.info("HodorBigdataActuator starting");
-        actuatorManager.start();
+        Thread thread = new Thread(() -> {
+            try {
+                actuatorManager.start();
+                latch.await();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+        thread.setDaemon(false);
+        thread.setName("hodor-agent-server");
+        thread.start();
+
         log.info("HodorBigdataActuator starting success");
         // add close shutdown hook
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             log.info("HodorBigdataActuator closed");
             actuatorManager.close();
+            latch.countDown();
         }));
     }
 
