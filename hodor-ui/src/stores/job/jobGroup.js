22f5e0a5003f135b48712a0f6485e6d0905a653b
tincopper <136082619@qq.com>
2023-10-29 22:33:30 +0800

    add breadcrumb in layout
    (cherry picked from commit b5ab9980cf7d5fb854a50671c3a19bf416518dba)

--- hodor-ui/src/stores/job/jobGroup.js
+++ hodor-ui/src/stores/job/jobGroup.js
@@ -3,39 +3,58 @@ import { ref, reactive } from 'vue'
 import { queryGroupListPagingAPI, getBindListAPI } from '@/apis/job/jobGroup'
 
 export const useJobGroupStore = defineStore('jobGroup', () => {
-    const allGroupList=ref([]);
-    const bindingList = ref({});
-    const paginationOpt=reactive({
-        pageNo:1,
-        pageSize:10,
-        total:0,
+  const allGroupList=ref([]);
+  const groupList=ref([]);
+  const bindingList = ref({});
+  const paginationOpt=reactive({
+    defaultCurrent:1,
+    defaultPageSize:10,
+    total:0,
+    onChange: (current, size) => {
+      paginationOpt.defaultCurrent = current;
+      paginationOpt.defaultPageSize = size;
+      getGroupListPaging();
+    },
+  })
+
+  const getGroupListPaging=async()=>{
+    const {defaultCurrent,defaultPageSize}=paginationOpt;
+    const res=await queryGroupListPagingAPI({pageNo:defaultCurrent,pageSize:defaultPageSize});
+    groupList.value=res.data.rows;
+    const {total}=res.data;
+    Object.assign(paginationOpt,{
+      ...paginationOpt,
+      total,
     })
-    const getGroupListPaging=async()=>{
-        const res=await queryGroupListPagingAPI({pageNo:1,pageSize:10});
-        const {total}=res.data;
-        Object.assign(paginationOpt,{
-            ...paginationOpt,
-            total,
-        })
-    }
-    const getAllGroupList=async()=>{
-        await getGroupListPaging();
-        const{total}=paginationOpt;
-        const res=await queryGroupListPagingAPI({pageNo:1,pageSize:total});
-        allGroupList.value=res.data.rows;
-    }
+  }
+  const getAllGroupList=async()=>{
+    await getGroupListPaging();
+    const{total}=paginationOpt;
+    const res=await queryGroupListPagingAPI({pageNo:1,pageSize:total});
+    allGroupList.value=res.data.rows;
+  }
+  // 删除分组
+  const deleteGroup=async (id)=>{
+    await handleDeleteGroupInfo(id).then(res=>{
+      groupList.value=groupList.value.filter(item=>item.id!==id)
+    });
+  }
 
-    const getBindingList = async () => {
-        const res = await getBindListAPI()
-        for (let bind of res.data) {
-            bindingList.value[bind['groupName']] = bind['clusterName']
-        }
+  const getBindingList = async () => {
+    const res = await getBindListAPI()
+    for (let bind of res.data) {
+      bindingList.value[bind['groupName']] = bind['clusterName']
     }
+  }
 
-    return {
-        allGroupList,
-        bindingList,
-        getAllGroupList,
-        getBindingList,
-    }
+  return {
+    allGroupList,
+    groupList,
+    paginationOpt,
+    getGroupListPaging,
+    getAllGroupList,
+    deleteGroup,
+    bindingList,
+    getBindingList,
+  }
 })
